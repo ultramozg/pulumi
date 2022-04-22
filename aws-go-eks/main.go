c8d6914f820bc9bed858f7611a1204c4c941e8ce
Dmitri <d.candu@hotmail.com>
2022-04-22 12:35:45 +0300

    create VPC

--- aws-go-eks/main.go
+++ aws-go-eks/main.go
@@ -27,7 +27,47 @@ func main() {
 				return err
 			}
 		*/
-		opt, err := network.createVpc(ctx)
+		vpc, err := ec2.NewVpc(ctx, "eks-vpc", &ec2.VpcArgs{
+			CidrBlock: pulumi.String("10.0.0.0/16"),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("app-dev-vpc1"),
+			},
+		})
+
+		gateway, err := ec2.NewInternetGateway(ctx, "app-dev-gw", &ec2.InternetGatewayArgs{
+			VpcId: vpc.ID(),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("app-dev-gw1"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = ec2.NewRouteTable(ctx, "app-dev-rt", &ec2.RouteTableArgs{
+			VpcId: vpc.ID(),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("app-dev-rt1"),
+			},
+			Routes: ec2.RouteTableRouteArray{
+				&ec2.RouteTableRouteArgs{
+					CidrBlock: pulumi.String("0.0.0.0/0"),
+					GatewayId: gateway.ID(),
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		subnet, err := ec2.NewSubnet(ctx, "app-dev-subnet1", &ec2.SubnetArgs{
+			VpcId:            vpc.ID(),
+			CidrBlock:        pulumi.String("10.0.2.0/24"),
+			AvailabilityZone: pulumi.String("ap-northeast-1b"),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("app-dev-subnet1"),
+			},
+		})
 		if err != nil {
 			return err
 		}
@@ -227,10 +267,6 @@ func main() {
 
 		return nil
 	})
-}
-
-func createVpc(ctx *pulumi.Context) {
-	panic("unimplemented")
 }
 
 //Create the KubeConfig Structure as per https://docs.aws.amazon.com/eks/latest/userguide/create-kubeconfig.html
