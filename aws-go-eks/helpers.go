4c1bc7d447463a6152e5fee94cccfb20589e60c5
dmitri.candu <d.candu@hotmail.com>
2022-05-21 13:37:53 +0300

    move out helpers methods to it own file

---
+++ aws-go-eks/helpers.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"crypto/sha1"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"net/url"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+//Create the KubeConfig Structure as per https://docs.aws.amazon.com/eks/latest/userguide/create-kubeconfig.html
+func generateKubeconfig(clusterEndpoint pulumi.StringOutput, certData pulumi.StringOutput, clusterName pulumi.StringOutput) pulumi.StringOutput {
+	return pulumi.Sprintf(`{
+        "apiVersion": "v1",
+        "clusters": [{
+            "cluster": {
+                "server": "%s",
+                "certificate-authority-data": "%s"
+            },
+            "name": "kubernetes",
+        }],
+        "contexts": [{
+            "context": {
+                "cluster": "kubernetes",
+                "user": "aws",
+            },
+            "name": "aws",
+        }],
+        "current-context": "aws",
+        "kind": "Config",
+        "users": [{
+            "name": "aws",
+            "user": {
+                "exec": {
+                    "apiVersion": "client.authentication.k8s.io/v1alpha1",
+                    "command": "aws",
+                    "args": [
+						"--region",
+						"eu-west-1",
+						"--profile",
+						"my-admin-account",
+						"eks",
+						"get-token",
+                        "--cluster-name",
+                        "%s",
+                    ],
+                },
+            },
+        }],
+    }`, clusterEndpoint, certData, clusterName)
+}
+
+func toPulumiStringArray(a []string) pulumi.StringArrayInput {
+	var res []pulumi.StringInput
+	for _, s := range a {
+		res = append(res, pulumi.String(s))
+	}
+	return pulumi.StringArray(res)
+}
+
+func getThumbprint(oidc_issuer string) (string, error) {
+	url, err := url.Parse(oidc_issuer)
+	if err != nil {
+		return "", err
+	}
+
+	conn, err := tls.Dial("tcp", url.Hostname()+":443", &tls.Config{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	cs := conn.ConnectionState()
+	numCerts := len(cs.PeerCertificates)
+	var root *x509.Certificate
+	// Important! Get the last cert in the chain, which is the root CA.
+	if numCerts >= 1 {
+		root = cs.PeerCertificates[numCerts-1]
+	} else {
+		return "", errors.New("Error getting cert list from connection \n")
+	}
+	// print out the fingerprint
+	return fmt.Sprintf("%x", sha1.Sum(root.Raw)), nil
+}
