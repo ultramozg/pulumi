5e6bfc411eff040b6517dda8725924bac1dbc5ad
Dmitri Candu <dmitri.candu@planetpayment.com>
2022-05-26 11:08:53 +0300

    add helm deployment for the aws load balancer controller

--- aws-go-eks/deployments.go
+++ aws-go-eks/deployments.go
@@ -7,10 +7,7 @@ import (
 
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/iam"
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
 	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/helm/v3"
-	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -70,39 +67,18 @@ func setupDeployments(ctx *pulumi.Context, eksResources *eksResources) error {
 	})
 	fmt.Println(albRole)
 
-	// Junk staff
-	namespace, err := corev1.NewNamespace(ctx, "app", &corev1.NamespaceArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name: pulumi.String("app"),
+	_, err = helm.NewChart(ctx, "aws-load-balancer-controller", helm.ChartArgs{
+		Chart:     pulumi.String("aws-load-balancer-controller"),
+		Version:   pulumi.String("1.4.1"),
+		Namespace: pulumi.String("kube-system"),
+		FetchArgs: helm.FetchArgs{
+			Repo: pulumi.String("https://aws.github.io/eks-charts"),
 		},
-	}, pulumi.Provider(eksResources.k8sProvider))
-	if err != nil {
-		return err
-	}
-
-	appLabels := pulumi.StringMap{
-		"app": pulumi.String("nginx"),
-	}
-	_, err = appsv1.NewDeployment(ctx, "app-dep", &appsv1.DeploymentArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Namespace: namespace.Metadata.Elem().Name(),
-		},
-		Spec: appsv1.DeploymentSpecArgs{
-			Selector: &metav1.LabelSelectorArgs{
-				MatchLabels: appLabels,
-			},
-			Replicas: pulumi.Int(1),
-			Template: &corev1.PodTemplateSpecArgs{
-				Metadata: &metav1.ObjectMetaArgs{
-					Labels: appLabels,
-				},
-				Spec: &corev1.PodSpecArgs{
-					Containers: corev1.ContainerArray{
-						corev1.ContainerArgs{
-							Name:  pulumi.String("nginx"),
-							Image: pulumi.String("nginx"),
-						}},
-				},
+		Values: pulumi.Map{
+			"clusterName": eksResources.eksCluster.Name,
+			"serviceAccount": pulumi.Map{
+				"create": pulumi.String("true"),
+				"name":   pulumi.String("aws-load-balancer-controller"),
 			},
 		},
 	}, pulumi.Provider(eksResources.k8sProvider))
@@ -110,34 +86,76 @@ func setupDeployments(ctx *pulumi.Context, eksResources *eksResources) error {
 		return err
 	}
 
-	service, err := corev1.NewService(ctx, "app-service", &corev1.ServiceArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Namespace: namespace.Metadata.Elem().Name(),
-			Labels:    appLabels,
-		},
-		Spec: &corev1.ServiceSpecArgs{
-			Ports: corev1.ServicePortArray{
-				corev1.ServicePortArgs{
-					Port:       pulumi.Int(80),
-					TargetPort: pulumi.Int(80),
-				},
+	/*
+		// Junk staff
+		namespace, err := corev1.NewNamespace(ctx, "app", &corev1.NamespaceArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String("app"),
 			},
-			Selector: appLabels,
-			Type:     pulumi.String("LoadBalancer"),
-		},
-	}, pulumi.Provider(eksResources.k8sProvider))
-	if err != nil {
-		return err
-	}
-
-	ctx.Export("url", service.Status.ApplyT(func(status *corev1.ServiceStatus) *string {
-		ingress := status.LoadBalancer.Ingress[0]
-		if ingress.Hostname != nil {
-			return ingress.Hostname
+		}, pulumi.Provider(eksResources.k8sProvider))
+		if err != nil {
+			return err
 		}
-		return ingress.Ip
-	}))
-	// end of the junk staff
+
+		appLabels := pulumi.StringMap{
+			"app": pulumi.String("nginx"),
+		}
+		_, err = appsv1.NewDeployment(ctx, "app-dep", &appsv1.DeploymentArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Namespace: namespace.Metadata.Elem().Name(),
+			},
+			Spec: appsv1.DeploymentSpecArgs{
+				Selector: &metav1.LabelSelectorArgs{
+					MatchLabels: appLabels,
+				},
+				Replicas: pulumi.Int(1),
+				Template: &corev1.PodTemplateSpecArgs{
+					Metadata: &metav1.ObjectMetaArgs{
+						Labels: appLabels,
+					},
+					Spec: &corev1.PodSpecArgs{
+						Containers: corev1.ContainerArray{
+							corev1.ContainerArgs{
+								Name:  pulumi.String("nginx"),
+								Image: pulumi.String("nginx"),
+							}},
+					},
+				},
+			},
+		}, pulumi.Provider(eksResources.k8sProvider))
+		if err != nil {
+			return err
+		}
+
+		service, err := corev1.NewService(ctx, "app-service", &corev1.ServiceArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Namespace: namespace.Metadata.Elem().Name(),
+				Labels:    appLabels,
+			},
+			Spec: &corev1.ServiceSpecArgs{
+				Ports: corev1.ServicePortArray{
+					corev1.ServicePortArgs{
+						Port:       pulumi.Int(80),
+						TargetPort: pulumi.Int(80),
+					},
+				},
+				Selector: appLabels,
+				Type:     pulumi.String("LoadBalancer"),
+			},
+		}, pulumi.Provider(eksResources.k8sProvider))
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("url", service.Status.ApplyT(func(status *corev1.ServiceStatus) *string {
+			ingress := status.LoadBalancer.Ingress[0]
+			if ingress.Hostname != nil {
+				return ingress.Hostname
+			}
+			return ingress.Ip
+		}))
+		// end of the junk staff
+	*/
 
 	return nil
 }
