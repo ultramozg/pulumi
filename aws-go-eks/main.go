f45d1e0760c14192ab451f233901d775e798e43f
Dmitri Candu <dmitri.candu@planetpayment.com>
2022-06-03 15:33:49 +0300

    now subnet config is reading from the configuration file

--- aws-go-eks/main.go
+++ aws-go-eks/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
+
+type networkData struct {
+	Vpc           string
+	PublicSubnets []struct {
+		Name string
+		Cidr string
+	}
+	PrivateSubnets []struct {
+		Name string
+		Cidr string
+	}
+}
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		netResources, err := setupNetwork(ctx)
+		var networkConfig networkData
+
+		conf := config.New(ctx, "")
+		conf.RequireObject("network", &networkConfig)
+
+		netResources, err := setupNetwork(ctx, &networkConfig)
 		if err != nil {
 			return err
 		}
