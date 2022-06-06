82af5e47665ff7ed06f263e4f5b5a30ca4d4494a
Dmitri Candu <dmitri.candu@planetpayment.com>
2022-06-06 10:31:02 +0300

    move out hardcoded part for eks to stack config as well

--- aws-go-eks/main.go
+++ aws-go-eks/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
@@ -17,19 +19,48 @@ type networkData struct {
 	}
 }
 
+type eksConfig struct {
+	NodeGroup struct {
+		CapacityType string
+		NodeType     string
+		Scaling      struct {
+			Desire int
+			Min    int
+			Max    int
+		}
+	}
+	Sg struct {
+		Ingress []struct {
+			Protocol string
+			FromPort int
+			ToPort   int
+			Cidr     string
+		}
+		Egress []struct {
+			Protocol string
+			FromPort int
+			ToPort   int
+			Cidr     string
+		}
+	}
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		var networkConfig networkData
+		var eksConfig eksConfig
 
 		conf := config.New(ctx, "")
 		conf.RequireObject("network", &networkConfig)
+		conf.RequireObject("eks", &eksConfig)
+		fmt.Println(eksConfig)
 
 		netResources, err := setupNetwork(ctx, &networkConfig)
 		if err != nil {
 			return err
 		}
 
-		eksResources, err := setupEKS(ctx, netResources)
+		eksResources, err := setupEKS(ctx, netResources, &eksConfig)
 		if err != nil {
 			return err
 		}
