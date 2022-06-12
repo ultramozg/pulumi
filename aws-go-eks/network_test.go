8f92ee373908b0d3f8a89f1ce357860ceee1a0d0
dmitri.candu <d.candu@hotmail.com>
2022-06-12 18:27:49 +0300

    done with network tests

--- aws-go-eks/network_test.go
+++ aws-go-eks/network_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+	"sync"
 	"testing"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
@@ -22,19 +24,46 @@ func (mocks) Call(args pulumi.MockCallArgs) (resource.PropertyMap, error) {
 func TestSetupNetwork(t *testing.T) {
 	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
 
-		_, err := setupNetwork(ctx, &networkData{"test-vpc", []subnetConfig{{"public", "192.168.0.0/24"}}, []subnetConfig{{"private", "192.168.1.1/24"}}})
+		networkConfigInput := networkData{"test-vpc", []subnetConfig{{"public", "192.168.0.0/24"}}, []subnetConfig{{"private", "192.168.1.0/24"}}}
+
+		network, err := setupNetwork(ctx, &networkConfigInput)
 		assert.NoError(t, err)
 
-		/*
-			var wg sync.WaitGroup
-			wg.Add(3)
+		var wg sync.WaitGroup
+		wg.Add(3)
 
-			// TODO(check 1): VPC has name
-			// TODO(check 2): One public subnet with CIDR
-			// TODO(check 3): One private subnet with CIDR
+		// TODO(check 1): VPC has name
+		network.vpc.Tags.ApplyT(func(tags map[string]string) error {
+			if v, ok := tags["Name"]; ok {
+				assert.Equal(t, strings.HasPrefix(v, "pulumi-eks-go"), true, "The Name should start with the prefix")
+			} else {
+				t.Fail()
+				t.Log("The VPC doesn't have a name")
+			}
 
-			wg.Wait()
-		*/
+			wg.Done()
+			return nil
+		})
+
+		// TODO(check 2): One public subnet with CIDR
+		assert.Equal(t, len(network.pubSubnets), 1, "Public subnets should have only one subnet")
+		network.pubSubnets[0].CidrBlock.ApplyT(func(cidrPtr *string) error {
+			cidr := *cidrPtr
+			assert.Equal(t, cidr, networkConfigInput.PublicSubnets[0].Cidr, "The public subnet should have the following cidr block")
+			wg.Done()
+			return nil
+		})
+
+		// TODO(check 3): One private subnet with CIDR
+		assert.Equal(t, len(network.privSubnets), 1, "Private subnets should have only one subnet")
+		network.privSubnets[0].CidrBlock.ApplyT(func(cidrPtr *string) error {
+			cidr := *cidrPtr
+			assert.Equal(t, cidr, networkConfigInput.PrivateSubnets[0].Cidr, "The public subnet should have the following cidr block")
+			wg.Done()
+			return nil
+		})
+
+		wg.Wait()
 		return nil
 	}, pulumi.WithMocks("project", "stack", mocks(0)))
 	assert.NoError(t, err)
